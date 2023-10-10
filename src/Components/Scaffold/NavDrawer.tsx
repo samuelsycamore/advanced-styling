a0693fb489b0db189d21f29fb046175039133d85
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2023-10-09 21:30:14 -0500

    appbar, drawer

--- src/Components/Scaffold/NavDrawer.tsx
+++ src/Components/Scaffold/NavDrawer.tsx
@@ -1,9 +1,39 @@
+import AppBar from '@mui/material/AppBar';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 
 
 export default function NavDrawer() {
     return (
         <div>
-            Nav Drawer
+            <AppBar position="fixed">
+              <Toolbar>
+                <Typography variant="h6" noWrap>
+                  Material UI styling
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Drawer variant="temporary" open={true}>
+              <List>
+                <ListItem>
+                  <ListItemText primary="Home" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="About" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Contact" />
+                </ListItem>
+              </List>
+            </Drawer>
+            <main>
+
+            </main>
         </div>
     )
 }
